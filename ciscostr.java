54b7c4016f8c25202b3f3d1ec97d4e0a0ccfac87
lkotaar1 <31516745+lkotaar1@users.noreply.github.com>
2019-04-16 13:13:35 -0400

    Create ciscostr.java

---
+++ ciscostr.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        String str = "abababcddee";
+        int[] arr = new int[26];
+        for(int i=0; i<str.length(); i++){
+            arr[str.charAt(i)-'a'] += 1;
+        }
+        
+        for(int i=0; i<str.length(); i++){
+            if(arr[str.charAt(i)]== 1){
+                System.out.println(str.charAt(i));
+            }
+        }
+     }
+}
