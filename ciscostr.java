e1710dde2a8c70d6c7253b5a3f6e0f31402bb834
lkotaar1 <31516745+lkotaar1@users.noreply.github.com>
2019-04-16 13:49:01 -0400

    Update ciscostr.java

--- ciscostr.java
+++ ciscostr.java
@@ -14,3 +14,29 @@ public class HelloWorld{
         }
      }
 }
+/*--------------------------------using Linked HashMap-------------------------------------*/
+
+import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+        String str = "abcabcefefaaaaaaaaaaaaaaaaaaaaaad";
+        Map<Character,Integer> m = new LinkedHashMap<Character,Integer>();
+        for(int i=0; i<str.length(); i++){
+          if(m.containsKey(str.charAt(i))){
+             m.put(str.charAt(i) , m.get(str.charAt(i))+1);
+              
+          }else{
+               m.put(str.charAt(i),1);
+          }
+        }
+        
+        
+        for(Character x : m.keySet()){
+            if(m.get(x) == 1){
+                System.out.println(x);
+                return;
+            }
+        }
+     }
+}
