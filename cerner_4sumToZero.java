f4e79ca324fc1230910c51bcf1558ab340e2cc0c
lkotaar1 <31516745+lkotaar1@users.noreply.github.com>
2019-04-28 22:02:42 -0400

    Update cerner_4sumToZero.java

--- cerner_4sumToZero.java
+++ cerner_4sumToZero.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class direction {
 	public static void main(String[] args){
-		 int[] arr = new int[] {1,-2,-1,-3,0,-1,-3,1,3,2};
+		 int[] arr = new int[] {1, 0, -1, 0, -2, 2};
 		 int count =0;
 		 int sum = 0;
 		 int flag = 0;
@@ -9,20 +9,14 @@ public class direction {
 		 for(int i=0;i<arr.length;i++){
 			 for(int j=i+1; j<arr.length;j++){
 				 for(int k=j+1; k<arr.length;k++) {
-					 if(m.containsKey(arr[k])){
-						 if(flag == 0) {
-							 count += 1; 
-						 }						 
-					 }else {	
-						 sum = arr[i]+arr[j]+arr[k];
-						 m.put(sum*-1, arr[k]);
+					 for(int l=k+1; l<arr.length; l++) {
+						 if(arr[i]+arr[j]+arr[k]+arr[l] == 0) {
+							 count += 1;
+						 }
 					 }
-					 if(k == arr.length - 1) {
-						 flag = 1;
-					 }
-					
 				 }
 			 }
 		 }
+		 System.out.println(count);
 	}
 }
